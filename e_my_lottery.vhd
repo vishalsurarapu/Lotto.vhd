49a07e924511fe8545d3341e17bcb5a97b05ff30
manaranan85 <67751684+manaranan85@users.noreply.github.com>
2020-07-24 21:41:25 +0200

    Update e_my_lottery.vhd
    added the generic counter component and clock_50  input.

--- e_my_lottery.vhd
+++ e_my_lottery.vhd
@@ -1,7 +1,8 @@
 library ieee; 
 use ieee.std_logic_1164.all; 
 entity e_my_lotto is 
-port( SW : in std_logic_vector (9 downto 0);
+port(   CLOCK_50 :in std_logic; 
+	SW : in std_logic_vector (9 downto 0);
 	KEY: in std_logic_vector(3 downto 0); 
 	LEDR: out std_logic_vector(9 downto 0); 
 	HEX0:		out				std_logic_vector(0 to 6);
@@ -23,7 +24,13 @@ component e_winning_FSM is
             LEDR_Sginal        : out   std_logic_vector(9 downto 0));  
 end component e_winning_FSM;
 
-
+component e_modulo_counter_er is
+	generic( n: natural := 4; k: integer := 15);
+	port (	        sl_clock, sl_reset_n:	in std_logic;
+			sl_enable:  in	std_logic;
+			slv_Q:	    out     std_logic_vector(n-1 downto 0);
+			sl_rollover: out std_logic );
+end component e_modulo_counter_er;
 
 
 
